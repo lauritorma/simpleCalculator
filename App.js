6f113625f3aa3c2e05d8815e2b5be9442ba804b9
Lauri Törmä <bgu171@myy.haaga-helia.fi>
2023-01-19 20:19:59 +0200

    implemented simple calculator

--- App.js
+++ App.js
@@ -1,11 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [first, setFirst] = useState('');
+  const [second, setSecond] = useState('');
+  const [result, setResult] = useState('');
+
+  const addPressed = () => {
+    setResult(parseInt(first) + parseInt(second))
+  }
+
+  const reductPressed = () => {
+    setResult(first - second)
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Result: {result}</Text>
+      <TextInput
+      style={{width: 200, borderColor: 'gray', borderWidth: 1, margin:10}}
+      keyboardType={"number-pad"}
+      onChangeText={first => setFirst(first)}
+      value={first}
+      />
+
+      <TextInput
+      style={{width: 200, borderColor: 'gray', borderWidth: 1, margin:10}}
+      keyboardType={"number-pad"}
+      onChangeText={second => setSecond(second)}
+      value={second}
+      />
+     
+     <View style={{flex:0, flexDirection: 'row', alignItems: 'center'}}>
+     <View style={{flex: 0.2, margin: 10}}>
+     <Button
+      title='+'
+      onPress={addPressed}
+      />
+      </View>
+      <View style={{flex: 0.2, margin: 10}}>
+      <Button
+      
+      title='-'
+      onPress={reductPressed}
+      />
+      </View>
+      </View>
     </View>
   );
 }
